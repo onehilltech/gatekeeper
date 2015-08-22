fb87af925845e890a617f02234f22532edb35a34
hilljh82 <hilljh82@gmail.com>
2015-08-22 15:11:34 -0400

    Removed oauth2 config in favor of server.locals

--- app/configs/oauth2.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = exports = {
-  client_id: "55abd83c57526d992769a4e7",
-  client_secret: "47ZM8YLJPfdevKxUlApLyNQharYmh5XbTJxMa2VdunFfG_qIekIGrQoOFxzx96aowjSPzt_1bhxNyBAeTcWf9pZK8E-foZifTIGU79qWKzOtML5eTPjuRL-PBTQLGjDEvZ9zYZuH6SXdoLzUBeCWJxNZy-EVHwtW9-ikOC88PTU"
-};
