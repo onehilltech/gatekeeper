612a2beca63c6fcd99d777522f57a95cd9b4cf1e
James H. Hill <hilljh82@gmail.com>
2018-04-28 01:54:19 -0400

    Initial port of password routes

---
+++ app/controllers/password.js
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const {
+  Action,
+  Controller,
+  model,
+  BadRequestError
+} = require ('@onehilltech/blueprint');
+
+const PasswordResetTokenGenerator = require ('../-internal/token-generators/password-reset');
+
+module.exports = Controller.extend ({
+  tokenGenerator: null,
+
+  init () {
+    this._super.call (this, ...arguments);
+
+    const config = this.app.lookup ('config:gatekeeper');
+    const tokenConfig = config.token;
+
+    this.tokenGenerator = new PasswordResetTokenGenerator (tokenConfig);
+  },
+
+  forgotPassword () {
+    return Action.extend ({
+      schema: {
+        email: {
+          in: 'body',
+          notEmpty: {
+            errorMessage: 'The request is missing the email parameter.'
+          },
+          isEmail: {
+            errorMessage: 'The provide email address is not valid.'
+          }
+        }
+      },
+
+      Account: model ('account'),
+
+      execute (req, res) {
+        const {email} = req.body;
+
+        return this.Account.findOne ({email}).then (account => {
+          if (!account)
+            return callback (new BadRequestError ('unknown_account', 'The account for the email address does not exist.'));
+
+          this.controller.app.emit ('gatekeeper.password.forgot', account);
+          res.status (200).json (true);
+        });
+      }
+    });
+  },
+
+  resetPassword () {
+    return Action.extend ({
+      Account: model ('account'),
+
+      schema: {
+        'reset-password.token': {
+          in: 'body',
+          notEmpty: {
+            errorMessage: 'The request is missing the token parameter.'
+          }
+        },
+        'reset-password.password': {
+          in: 'body',
+          notEmpty: {
+            errorMessage: 'The request is missing the password parameter.'
+          }
+        }
+      },
+
+      execute (req, res) {
+        let {token,password} = req.body['reset-password'];
+
+        return this.controller.tokenGenerator.verifyToken (token)
+          .then (payload => this.Account.findOne ({email: payload.email}))
+          .then (account => {
+            account.password = password;
+            return account.save ();
+          })
+          .then (account => {
+            this.controller.app.emit ('gatekeeper.password.reset', account);
+
+            res.status (200).json (true);
+          });
+      }
+    });
+  }
+});
--- tests/unit-tests/app/routers/v1/PasswordRouterTest.js
+++ tests/unit-tests/app/routers/v1/PasswordRouterTest.js
@@ -1,47 +1,64 @@
+/*
+ * Copyright (c) 2018 One Hill Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const {expect} = require ('chai');
 const blueprint = require ('@onehilltech/blueprint');
-const async = require ('async');
-//const ResetPasswordTokenGenerator = require ('../../../../../app/utils/reset-password-token-generator');
 
-describe ('app | routers | password', function () {
+const {
+  seed
+} = require ('@onehilltech/blueprint-mongodb');
+
+const {
+  request
+} = require ('../../../../../lib/testing');
+
+const async = require ('async');
+const ResetPasswordTokenGenerator = require ('../../../../../app/-internal/token-generators/password-reset');
+
+describe.only ('app | routers | password', function () {
   let tokenGenerator;
 
   before (function () {
-    tokenGenerator = new ResetPasswordTokenGenerator ();
+    const config = blueprint.lookup ('config:gatekeeper');
+    tokenGenerator = new ResetPasswordTokenGenerator (config.token);
   });
 
   describe ('/v1/password/forgot', function () {
     describe ('POST', function () {
-      it ('should initiate forgot password sequence', function (done) {
-        let account = blueprint.app.seeds.$default.accounts[0];
+      it ('should initiate forgot password sequence', function () {
+        let {accounts} = seed ('$default');
+        const account = accounts[0];
 
-        blueprint.messaging.once ('gatekeeper.password.forgot', (acc) => {
-          expect (acc.id).to.equal (account.id);
-          return done (null);
-        });
-
-        blueprint.testing.request ()
+        return request ()
           .post ('/v1/password/forgot')
           .send ({email: account.email})
-          .expect (200, 'true').end (function (err) {
-            if (err) return done (err);
-        });
+          .expect (200, 'true');
       });
     });
   });
 
   describe ('/v1/password/reset', function () {
     describe ('POST', function () {
-      it ('should reset the account password', function (done) {
-        let account = blueprint.app.seeds.$default.accounts[0];
+      it ('should reset the account password', function () {
+        let {accounts} = seed ('$default');
+        const account = accounts[0];
 
-        async.waterfall ([
-          function (callback) {
-            tokenGenerator.generateToken ({email: account.email}, {}, callback);
-          },
-
-          function (token, callback) {
-            blueprint.messaging.once ('gatekeeper.password.reset', (acc) => {
+        return tokenGenerator.generateToken ({email: account.email})
+          .then (token => {
+            blueprint.once ('gatekeeper.password.reset', (acc) => {
               expect (acc.id).to.equal (account.id);
               expect (acc.password).to.not.equal ('1234567890');
 
@@ -57,14 +74,11 @@ describe ('app | routers | password', function () {
               ], callback);
             });
 
-            blueprint.testing.request ()
+            request ()
               .post ('/v1/password/reset')
               .send ({'reset-password': {token: token, password: '1234567890'}})
-              .expect (200, 'true').end ((err) => {
-                if (err) return callback (err);
-            });
-          }
-        ], done);
+              .expect (200, 'true');
+          });
       });
     });
   });
